92e94fc61f2398ad2767e11534ca7474112ce109
Nima Koohkhezri <nimakoohkhezri@gmail.com>
2024-07-07 17:07:52 +0330

    get questions added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <QMessageBox>
+#include "TriviaAPI.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -33,5 +34,15 @@ void MainWindow::on_pvp_start_btn_clicked()
 {
     int currentIndex = ui->stackedWidget->currentIndex();
     ui->stackedWidget->setCurrentIndex(currentIndex + 1);
+    TriviaAPI api;
+    Category category = api.category_identifier(ui->pvp_category_selection->currentText());
+    QString difficulty;
+    if (ui->pvp_easy_btn->isChecked()){
+        difficulty = "easy";
+    } else if (ui->pvp_medium_btn->isChecked()){
+        difficulty = "medium";
+    } else if (ui->pvp_hard_btn->isChecked()){
+        difficulty = "hard";
+    }
+    api.get_questions(category, difficulty);
 }
-
